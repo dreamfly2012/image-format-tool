90842036ff338d6b9d8dfaf3f2b775de9e54948c
menghuiguli <fujia0@qq.com>
2024-03-23 21:51:31 +0800

    gui build

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # image-format-tool
-batch format image 
+
+## 批量图片格式转换工具
+
+1. 安装依赖
+
+```shell
+pip install -r requirements.txt
+```
+
+2. 执行代码
+   
+```shell
+python app.py
+```
+
+
+
+
--- app.py
+++ app.py
@@ -1,36 +1,71 @@
 import os
 from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
 from PIL import UnidentifiedImageError
+import threading
 
-def convert_images():
-    format = choice.get()
-    print(format)
-    print(input_folder)
-    print(output_folder)
+
+def check_folder_exist(folder):
+    if not os.path.exists(folder):
+        return False
+    else:
+        return True
+    
+
+def thread_convert_images():
+    threads = []
+    if check_folder_exist(input_folder) == False:
+        tk.messagebox.showinfo("错误消息", "输入文件夹不存在")  
+        return
+    if check_folder_exist(output_folder) == False:
+        tk.messagebox.showinfo("错误消息", "输出文件夹不存在")  
+        return
     for filename in os.listdir(input_folder):
         if filename.lower().endswith(('.webp', '.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')):
-            try:
-                filepath = os.path.join(input_folder, filename)
-                output_filename, _ = os.path.splitext(filename)  # 分离文件名和扩展名
-                output_path = os.path.join(output_folder, output_filename + '.' + format)
-                img = Image.open(filepath)
-                # 转换格式，例如从PNG转换为JPG
-                img.save(output_path, format, optimize=True)
-                print(f"Converted {filename}")
-            except UnidentifiedImageError:
-                print(f"Cannot convert {filename}. File may be corrupt or not supported.")
+            thread = threading.Thread(target=format_images, args=([filename]))
+            thread.start()
+            threads.append(thread)
+    
+    # 等待所有线程执行完毕
+    for thread in threads:
+        thread.join()
+                   
+    # 全部执行之后，弹出提示框转换完成
+    tk.messagebox.showinfo("转换完成", "转换完成")  
+
+def convert_images():
+    thread = threading.Thread(target=thread_convert_images) 
+    thread.daemon = True
+    thread.start() 
+    
+    
+     
+    
+def format_images(filename):
+    format = choice.get()
+    try:
+        filepath = os.path.join(input_folder, filename)
+        output_filename, _ = os.path.splitext(filename)  # 分离文件名和扩展名
+        output_path = os.path.join(output_folder, output_filename + '.' + format)
+        img = Image.open(filepath)
+        # 转换格式，例如从PNG转换为JPG
+        if img.mode == "P":
+            img = img.convert('RGB')
+        img.save(output_path, format, optimize=True)
+        print(f"Converted {filename}")
+    except UnidentifiedImageError:
+        print(f"Cannot convert {filename}. File may be corrupt or not supported.")    
 
 def browse_input_folder():
     global input_folder
     input_folder = filedialog.askdirectory()
-    print(input_folder)
 
 def browse_output_folder():
     global output_folder
     output_folder = filedialog.askdirectory()
-    print(output_folder)
 
 def selected_format(format):
     format = choice.get()
@@ -40,21 +75,35 @@ def selected_format(format):
 root = Tk()
 root.title("Batch Image Converter")
 
+# 设置窗口大小
+root.geometry("800x600")
+
 choice = StringVar()
 input_folder = ""
 output_folder = ""
 
+format_list = ("PNG", "JPEG", "BMP", "GIF", "TIFF", "WEBP")
+#choice.set(format_list[0])
+
 # 添加控件
-input_folder_button = Button(root, text="Select Input Folder", command=browse_input_folder)
-input_folder_button.pack()
+# label 选择图片位置
+input_folder_label = ttk.Label(root, text="选择图片位置:")
+input_folder_label.grid(row=0,column=0,pady=10,padx=20)
+input_folder_button = ttk.Button(root, text="Select Input Folder", width=50,command=browse_input_folder)
+input_folder_button.grid(row=0,column=1,columnspan=2,pady=10,padx=20 )
 
-output_folder_button = Button(root, text="Select Output Folder", command= browse_output_folder)
-output_folder_button.pack()
+output_folder_label = ttk.Label(root, text="选择图片输出位置:")
+output_folder_label.grid(row=1,column=0,pady=10,padx=20)
+output_folder_button = ttk.Button(root, text="Select Output Folder", width=50,command= browse_output_folder)
+output_folder_button.grid(row=1,column=1,columnspan=2,pady=10,padx=20)
 
-format_menu = OptionMenu(root, choice, "PNG", "JPG", "BMP", "GIF", "TIFF", command=selected_format)
-format_menu.pack()
+# label 图片转换格式
+format_label = ttk.Label(root, text="图片转换格式:")
+format_label.grid(row=2,column=0,pady=10,padx=20)
+format_menu = ttk.OptionMenu(root, choice, "PNG" ,*format_list, command=selected_format)
+format_menu.grid(row=2,column=1,columnspan=2,pady=10,padx=20)
 
-convert_button = Button(root, text="Start Conversion", command= convert_images)
-convert_button.pack()
+convert_button = ttk.Button(root, text="开始转换", command= convert_images)
+convert_button.grid(row=3,column=0,columnspan=4)
 
 root.mainloop()
